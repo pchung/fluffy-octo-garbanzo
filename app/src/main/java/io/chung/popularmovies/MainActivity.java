eb579deda0e0df3036382d8db74ee71a163c2b15
patrick <patrick.chung@vw.com>
2017-07-05 12:02:46 -0700

    Add boilerplate for initializing RecyclerView

--- app/src/main/java/io/chung/popularmovies/MainActivity.java
+++ app/src/main/java/io/chung/popularmovies/MainActivity.java
@@ -2,12 +2,26 @@ package io.chung.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_LIST_ITEMS = 100;
+
+    /*
+     * Reference to RecyclerView.
+     */
+    RecyclerView mMovieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mMovieList = (RecyclerView) findViewById(R.id.rv_movie_list);
+        mMovieList.setLayoutManager(layoutManager);
+        mMovieList.setHasFixedSize(true);
     }
 }
